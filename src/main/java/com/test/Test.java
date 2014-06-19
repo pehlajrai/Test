29c5e0fed65317d8e5efc5b07e35ae939db4ccf0
Pehlaj Rai <prai@futurelab.ch>
2014-06-19 09:44:58 +0500

    Test.\R Added new file\nTest.java\n\RPackage com.test

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,17 @@
+package com.test;
+/**
+ * Filename Test.java
+ * Package  
+ * Project  fahrzeuge
+ */
+
+/**
+ * @author Pehlaj Rai
+ * @since 19 Jun 2014 09:42:27
+ */
+public class Test {
+
+	/*  */
+	public Test() {}
+
+}
